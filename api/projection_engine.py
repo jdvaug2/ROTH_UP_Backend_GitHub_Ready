d93fdf1a7236ab565d4113035acbd80426ad14c6
jdvaug2 <valorenadvisors@gmail.com>
2025-06-25 11:08:38 -0400

    Update projection_engine.py

--- api/projection_engine.py
+++ api/projection_engine.py
@@ -1,9 +1,12 @@
 from api.utils.tax_brackets import get_adjusted_brackets
 # In projection loop
 brackets = get_adjusted_brackets(
-    data.current_year, data.current_year, data.filing_status,
-    data.bracket_inflation_pct, data.bracket_rate_shift_pct,
+    data.current_year, 
+    data.current_year,
+    data.filing_status,
+    data.bracket_inflation_pct,
+    data.bracket_rate_shift_pct,
     data.custom_future_brackets
-)
+ )
 taxable_income = data.retirement_income_future + conversion + rmd
 tax = calculate_tax(taxable_income, brackets)
